ab8b4ddbd80d8dca9f7de19a41784878f0e29899
Station Chenqiao <i@cctv3.net>
2024-11-22 19:53:22 +0800

    - 移除`ant-design` ...

--- src/views/emailSetting/components/checkCode4ResetPassword.vue
+++ src/views/emailSetting/components/checkCode4ResetPassword.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Button, Input } from "ant-design-vue";
 import { ACTION } from "../constants";
 const emits = defineEmits(["onPress"]);
 </script>
@@ -7,10 +6,10 @@ const emits = defineEmits(["onPress"]);
 <template>
   <div>
     <div>
-      <Input placeholder="请输入邮箱" /><Input placeholder="请输入验证码" />
+      <n-input placeholder="请输入邮箱" /><Input placeholder="请输入验证码" />
     </div>
-    <Button type="primary" @click="emits('onPress', { action: ACTION.next })"
-      >下一步</Button
+    <n-button type="primary" @click="emits('onPress', { action: ACTION.next })"
+      >下一步</n-button
     >
   </div>
 </template>
--- src/views/emailSetting/components/selectMethod.vue
+++ src/views/emailSetting/components/selectMethod.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { Button } from "ant-design-vue";
-
 const emits = defineEmits(["onSelect"]);
 </script>
 
 <template>
   <div>
-    <Button type="default" @click="emits('onSelect', 0)">密码</Button>
-    <Button type="primary" @click="emits('onSelect', 1)">邮箱验证码</Button>
+    <n-button type="default" @click="emits('onSelect', 0)">密码</n-button>
+    <n-button type="primary" @click="emits('onSelect', 1)">邮箱验证码</n-button>
   </div>
 </template>
 
