a696e56b0de21d85d7c033af4692bc5c687d5503
Station Chenqiao <i@cctv3.net>
2025-02-16 03:00:32 +0800

    feat: 定制视频播放器 ...

--- src/views/testVideo/index.vue
+++ src/views/testVideo/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import Hls from "hls.js"; // 手动引入 HLS.js
+import Hls from "hls.js";
 import Plyr from "plyr";
 import { onMounted, onUnmounted, ref, watch } from "vue";
 import { useRoute } from "vue-router";
@@ -17,7 +17,29 @@ let hls: Hls | null = null;
 
 onMounted(() => {
   if (!videoRef.value) return;
-  player = new Plyr(videoRef.value, { autoplay: true });
+  player = new Plyr(videoRef.value, {
+    autoplay: true,
+    hideControls: false,
+    displayDuration: true,
+    storage: { enabled: false },
+    settings: ["captions", "quality", "speed"],
+    controls: [
+      "play", // 播放按钮
+      "progress", // 进度条
+      "current-time", // 当前时间
+      "mute", // 静音
+      // "volume", // 音量
+      // "captions", // 字幕
+      "settings",
+    ],
+    speed: {
+      selected: 1, // 默认倍速
+      options: [1.5, 2, 1], // 只显示 1x 和 1.5x
+    },
+    i18n: {
+      normal: "默认",
+    },
+  });
   const src = route.query.src as string | undefined;
   if (src) {
     loadVideo(src);
@@ -60,7 +82,7 @@ const loadVideo = (src: string) => {
       // Safari 原生支持 HLS
       videoRef.value.src = src;
     } else {
-      console.error("HLS.js 不支持当前浏览器");
+      alert("HLS.js 不支持当前浏览器");
     }
   } else {
     // 普通视频格式（MP4）
@@ -80,6 +102,9 @@ const loadVideo = (src: string) => {
   align-items: center;
   display: flex;
   overflow-y: hidden;
+  .plyr__control[data-plyr="airplay"] {
+    display: none !important;
+  }
 }
 
 video {
