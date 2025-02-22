7a35790f35e9db533262694f54aafb3029172029
孙宇鹏 <sunyp@bwoil.com>
2025-02-22 12:40:42 +0800

    fix: 优化DPlayer体验，始终显示小圆点儿 ...

--- src/views/testDPlayer/index.vue
+++ src/views/testDPlayer/index.vue
@@ -64,7 +64,7 @@ const initPlayer = (src: string) => {
         },
       },
     },
-    theme: '#ca6924',
+    theme: "#ca6924",
     contextmenu: [],
     playbackSpeed: [1, 1.5, 2],
     mutex: true,
@@ -117,6 +117,14 @@ const initPlayer = (src: string) => {
   /* 点进进度条某个位置，左下角浮动时间 快进/快退多少秒 */
   .dplayer-notice {
     display: none !important;
+  }
+  /* 进度条上的圆点，默认情况下只有精度条获得焦点才显示 */
+  .dplayer-controller
+    .dplayer-bar-wrap
+    .dplayer-bar
+    .dplayer-played
+    .dplayer-thumb {
+    transform: scale(1) !important;
   }
 }
 </style>
